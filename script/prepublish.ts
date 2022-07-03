af9d62aab08a165f6b75f86d12d1ef22040dea1d
Alvis Tse <alvistse123@gmail.com>
2022-07-03 19:43:02 +0800

    chore: simplify prepublish process

--- script/prepublish.ts
+++
@@ -1,9 +0,0 @@
-import { buildProcess } from './build-process'
-import standardVersion from 'standard-version'
-
-const prepublish = async () => {
-    await standardVersion()
-    await buildProcess()
-}
-
-prepublish()
