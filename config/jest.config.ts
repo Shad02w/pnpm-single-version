d67632cedac4ce3cbe47b9456f06a017500d63d3
Alvis Tse <alvistse123@gmail.com>
2022-07-05 17:32:35 +0800

    chore: udpated jest config

--- config/jest.config.ts
+++ config/jest.config.ts
@@ -3,12 +3,14 @@ import * as path from 'path'
 
 const config: Config.InitialOptionsWithRootDir = {
     preset: 'ts-jest',
-    rootDir: '../test',
+    rootDir: '../',
     globals: {
         'ts-jest': {
             tsconfig: path.join(__dirname, './tsconfig.test.json')
         }
     },
+    testEnvironment: 'node',
+    testRegex: [String.raw`\.test\.tsx?$`],
     transform: {
         [String.raw`\.(ts|tsx)$`]: 'ts-jest'
     }
