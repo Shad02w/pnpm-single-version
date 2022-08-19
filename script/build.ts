e4facc90b454bfd7100470dd586ddb3c5db3a8e2
Alvis Tse <alvistse123@gmail.com>
2022-08-20 02:25:37 +0800

    refactor: cleanup build script

--- script/build.ts
+++ script/build.ts
@@ -3,9 +3,6 @@ import fs from 'fs-extra'
 import path from 'path'
 
 const outputPath = path.join(__dirname, '../dist')
-const packageJSONFilePath = path.join(__dirname, '../package.json')
-const readMeFilePath = path.join(__dirname, '../README.md')
-const licenseFilePath = path.join(__dirname, '../LICENSE')
 const sourceFilePath = path.join(__dirname, '../src')
 const swcConfigFilePath = path.join(__dirname, '../config/.swcrc')
 const tsconfigFilePath = path.join(__dirname, '../config/tsconfig.src.json')
@@ -20,28 +17,11 @@ export const build = async () => {
     await fs.emptyDir(outputPath)
     console.log('> Clean output path')
 
-    await spawnSync(
-        'swc',
-        [sourceFilePath, '-d', outputPath, '--config-file', swcConfigFilePath],
-        spawnOptions
-    )
+    await spawnSync('swc', [sourceFilePath, '-d', outputPath, '--config-file', swcConfigFilePath], spawnOptions)
     console.log('> Transpiled')
 
-    await spawnSync(
-        'tsc',
-        ['-P', tsconfigFilePath, '--emitDeclarationOnly'],
-        spawnOptions
-    )
+    await spawnSync('tsc', ['-P', tsconfigFilePath, '--emitDeclarationOnly'], spawnOptions)
     console.log('> d.ts generated')
-
-    // await fs.copy(packageJSONFilePath, path.join(outputPath, 'package.json'))
-    // console.log('> Copied package.json')
-    //
-    // await fs.copy(readMeFilePath, path.join(outputPath, 'README.md'))
-    // console.log('> Copied README.md')
-    //
-    // await fs.copy(licenseFilePath, path.join(outputPath, 'LICENSE'))
-    // console.log('> Copied LICENSE')
 }
 
 build()
