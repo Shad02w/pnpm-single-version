0057fbb219c0871c837c83a17c3cb3182d0282cf
Alvis Tse <alvistse123@gmail.com>
2022-08-22 08:41:05 +0800

    fix: find package.json root when it is not a monorepo

--- src/parser-option/find-project-root.ts
+++ src/parser-option/find-project-root.ts
@@ -2,16 +2,17 @@ import findWorkspaceDir from '@pnpm/find-workspace-dir'
 import { realpath } from 'fs/promises'
 import findUp from 'find-up'
 import { ProjectRootError } from '../error'
+import path from 'path'
 
 /**
  * Can find the root path of the package and monorepo
  */
 export const findProjectRoot = async (): Promise<string> => {
-    const projectRoot =
-        (await findWorkspaceDir(process.cwd())) ??
-        findUp.sync(['package.json'], {
-            cwd: await realpath(process.cwd())
-        })
+    let projectRoot = await findWorkspaceDir(process.cwd())
+    if (!projectRoot) {
+        const packageJSONPath = await findUp('package.json', { cwd: await realpath(process.cwd()) })
+        projectRoot = packageJSONPath ? path.parse(packageJSONPath).dir : undefined
+    }
 
     if (!projectRoot) {
         throw new ProjectRootError()
