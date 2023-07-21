ac4fdf5f71cd83e2e06cd2fbfd43bf10239635ef
Alvis Tse <alvistse123@gmail.com>
2023-07-21 10:11:35 +0800

    chore: update jest config

--- config/jest.config.ts
+++ config/jest.config.ts
@@ -1,18 +1,19 @@
 import { Config } from '@jest/types'
 import * as path from 'path'
+import type { JestConfigWithTsJest } from 'ts-jest'
 
 const config: Config.InitialOptionsWithRootDir = {
     preset: 'ts-jest',
     rootDir: '../',
-    globals: {
-        'ts-jest': {
-            tsconfig: path.join(__dirname, './tsconfig.test.json')
-        }
-    },
     testEnvironment: 'node',
     testRegex: [String.raw`\.test\.tsx?$`],
     transform: {
-        [String.raw`\.(ts|tsx)$`]: 'ts-jest'
+        [String.raw`\.ts$`]: [
+            'ts-jest',
+            {
+                tsconfig: path.join(__dirname, './tsconfig.test.json')
+            }
+        ]
     }
 }
 
